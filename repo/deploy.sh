eef91ba9c5c2710c75b8aeac5d467e0d9f5a555a
Danny Glover <dcgm1986@gmail.com>
2017-01-07 14:15:29 +0000

    Updating NotToday10 depiction

--- repo/deploy.sh
+++ repo/deploy.sh
@@ -1,4 +1,10 @@
+rm Packages
 rm Packages.gz
 rm Packages.bz2
 dpkg-scanpackages Files/ > Packages
 gzip -k Packages
+git -C ../
+git add .
+git commit -m "$1"
+git push origin master
+git push origin2 master
